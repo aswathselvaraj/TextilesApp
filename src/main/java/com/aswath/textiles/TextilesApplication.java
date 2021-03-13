f946d6c5b668e9e7b7f319669a68cb6a83c85e1b
aswathselvaraj <64221612+aswathselvaraj@users.noreply.github.com>
2021-03-13 15:34:29 +0530

    first commit

---
+++ src/main/java/com/aswath/textiles/TextilesApplication.java
@@ -0,0 +1,12 @@
+package com.aswath.textiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextilesApplication {
+public static void main(String[] args){
+    SpringApplication.run(TextilesApplication.class);
+
+}
+}
